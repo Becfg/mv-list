d3ae25d7d93cf7d0dee96110f30a385e73dc77a0
becfg <1841507121@qq.com>
2024-04-10 11:14:28 +0800

    初始化路由

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import Navi from "./components/navi.vue";
+</script>
+
+<template>
+<navi> </navi>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/navi.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="nav">
+    <router-link to="">首页</router-link>
+    <router-link to="">频道列表</router-link>
+    <router-link to="">我的</router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.nav {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: row;
+  background-color: #2c3e50;
+  line-height: 50px;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+}
+
+</style>
