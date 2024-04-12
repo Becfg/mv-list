32066bd4eb5f0f95cd858616cabcf2acd968aadf
Treiiis <1841507121@qq.com>
2024-04-12 21:20:11 +0800

    1

---
+++ src/components/search.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import ch from "../assets/channels.json"
+
+const emit = defineEmits(["close"])
+</script>
+
+<template>
+  <div class="search-cont">
+    <div class="search-box">
+      <div class="search-bar">
+        <input type="text">
+        <span @click="$emit('close')">取消</span>
+      </div>
+      <div class="filter">
+        <div>影片</div>
+        <div>默认排序</div>
+        <div>全部频道</div>
+      </div>
+    </div>
+    <div class="filter-content">
+      <div class="item">全部频道</div>
+      <div class="item" v-for="item in ch">{{ item.name }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+* {
+  //border: 1px solid red;
+}
+
+.search-cont {
+  z-index: 10;
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background: rgb(0, 0, 0, 0.6);
+  color: #a2a2a2;
+
+  .search-box {
+    background: #232323;
+    padding-bottom: 10px;
+
+    .search-bar {
+      input {
+        margin: 5px;
+        margin-left: 20px;
+        height: 30px;
+        width: 80vw;
+      }
+    }
+
+    .filter {
+      display: flex;
+      justify-content: space-evenly;
+
+      div {
+        font-size: 20px;
+      }
+    }
+  }
+
+  .filter-content {
+    padding: 5px;
+    display: flex;
+    flex-wrap: wrap;
+    background-color: #171717;
+
+    .item {
+      margin-right: 18px;
+      margin-bottom: 5px;
+      border-radius: 5px;
+      padding: 3px;
+      background-color: #3b3b3b;
+    }
+  }
+}
+</style>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,20 +1,12 @@
 <script lang="ts" setup>
 
 import Card from "../components/card.vue";
+import mov from "../assets/movies.json"
 import img from "../assets/anime.webp"
-import { ref } from "vue";
-
-const movies = ref([
-  { id: 1, type: "动画", name: "迷宫饭", url: img },
-  { id: 2, type: "动画", name: "迷宫饭", url: img },
-  { id: 3, type: "动画", name: "迷宫饭", url: img },
-  { id: 4, type: "动画", name: "迷宫饭", url: img },
-  { id: 5, type: "动画", name: "迷宫饭", url: img },
-])
 </script>
 
 <template>
-  <card v-for="movie in movies" :key="movie.id" :MvName="movie.name" :type="movie.type" :url="movie.url"></card>
+  <card v-for="movie in mov" :key="movie.id" :MvName="movie.name" :type="movie.type" :url="img"></card>
 </template>
 
 <style scoped lang="scss">
